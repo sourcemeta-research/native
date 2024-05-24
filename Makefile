98717d426f9f493187939ba4a174d4a563e34e1a
Tony Gorez <gorez.tony@gmail.com>
2024-05-24 19:38:51 +0200

    test: add basic test
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- Makefile
+++ Makefile
@@ -8,6 +8,15 @@ configure: .always
 build: configure .always
 	$(CMAKE) --build ./build
 
+test: build
+	./build/example/hello_world/example_hello_world
+	@if [ $$? -eq 0 ]; then \
+		echo "Success: The program exited with status code 0"; \
+	else \
+		echo "Error: The program did not exit with status code 0"; \
+		exit 1; \
+	fi
+
 clean:
 	rm -rf ./build
 
--- example/hello_world/hello_world.cc
+++ example/hello_world/hello_world.cc
@@ -7,7 +7,10 @@ class App : public sourcemeta::native::Application {
 public:
   auto on_start() -> void override { std::cout << "Starting!" << std::endl; }
 
-  auto on_ready() -> void override { std::cout << "Ready!" << std::endl; }
+  auto on_ready() -> void override {
+    std::cout << "Ready!" << std::endl;
+    this->exit(0);
+  }
 
   auto on_error(std::exception_ptr) noexcept -> void override {}
 };
