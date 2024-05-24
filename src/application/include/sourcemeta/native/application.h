fb886207a1396c85ce841bd326d98fad851a7cc8
Tony Gorez <gorez.tony@gmail.com>
2024-05-24 20:10:52 +0200

    doc: add code doc
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- src/application/include/sourcemeta/native/application.h
+++ src/application/include/sourcemeta/native/application.h
@@ -20,21 +20,32 @@ public:
   Application(Application const &) = delete;
   void operator=(Application const &) = delete;
 
+  /// The Application class is designed to prevent multiple instances
+  /// of the application from being created by using a singleton pattern.
   static Application *instance();
 
-  template <typename T> static auto instance() -> T * {
-    static_assert(std::is_base_of_v<Application, T>);
-    Application *instance = Application::instance();
-    assert(instance);
-    return dynamic_cast<T *>(instance);
-  }
-
+  /// This method is responsible for starting the application.
+  /// It returns the exit code of the application. Instead of calling
+  /// this method directly, use the NATIVE_RUN macro.
   auto run() noexcept -> int;
-  auto exit(const int code) const noexcept -> void;
+
+  /// This method is responsible for gracefully shutting down the application.
+  auto exit(const int code = EXIT_SUCCESS) const noexcept -> void;
 
 protected:
+  /// Override this method to hook into the start event of the application.
+  /// This method is called once when the application starts, before any
+  /// other initialization.
   virtual auto on_start() -> void = 0;
+
+  /// Override this method to hook into the event when the graphical user
+  /// interface (GUI) is ready. In the context of a CLI application, this
+  /// method is called immediately after the on_start method.
   virtual auto on_ready() -> void = 0;
+
+  /// Override this method to handle errors that occur during the application's
+  /// execution. This method provides a centralized way to manage exceptions
+  /// and perform cleanup or logging.
   virtual auto on_error(std::exception_ptr error) -> void = 0;
 
 private:
