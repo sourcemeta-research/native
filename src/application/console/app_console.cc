ab1045e64480dbc3a8cfb96f94df2db29180765d
Tony Gorez <gorez.tony@gmail.com>
2024-10-30 10:07:35 +0100

    feat(app): add cli for windows (console) (#24)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

---
+++ src/application/console/app_console.cc
@@ -0,0 +1,30 @@
+#include <sourcemeta/native/application.h>
+
+namespace sourcemeta::native {
+
+Application::Application() {}
+
+Application::~Application() {}
+
+auto Application::run() noexcept -> int {
+  assert(!running_);
+  running_ = true;
+
+  try {
+    this->on_start();
+    // For CLI, on_ready is called right after on_start
+    // since there's no GUI to wait for
+    this->on_ready();
+  } catch (...) {
+    this->on_error(std::current_exception());
+  }
+
+  return 0;
+}
+
+auto Application::exit(const int code) const noexcept -> void {
+  assert(running_);
+  std::exit(code); // Direct exit for CLI
+}
+
+} // namespace sourcemeta::native
