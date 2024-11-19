dd1488262307dbb34df8310158c73984a2651923
Tony Gorez <gorez.tony@gmail.com>
2024-11-19 18:05:01 +0100

    refactor(ui/webview): absorb attach_to + nits
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- src/ui/webview/appkit/webview_appkit.mm
+++ src/ui/webview/appkit/webview_appkit.mm
@@ -21,12 +21,14 @@ public:
   auto load_url(const std::string &url) -> void {
     NSString *url_ns_str = [NSString stringWithUTF8String:url.c_str()];
     NSURL *url_ns = [NSURL URLWithString:url_ns_str];
-    if (url_ns) {
-      NSURLRequest *request = [NSURLRequest requestWithURL:url_ns];
-      [webView_ loadRequest:request];
-    } else {
+
+    if (!url_ns) {
       NSLog(@"Invalid URL string: %@", url_ns_str);
+      return;
     }
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:url_ns];
+    [webView_ loadRequest:request];
   }
 
   auto load_html(const std::string &html_path) -> void {
@@ -37,11 +39,21 @@ public:
     NSURL *html_url = [bundle URLForResource:html_filename_without_extension
                                withExtension:@"html"];
 
-    [this->get_webview() loadFileURL:html_url
-             allowingReadAccessToURL:[html_url URLByDeletingLastPathComponent]];
+    if (!html_url) {
+      NSLog(@"Failed to load HTML file: %@", html_filename);
+      return;
+    }
+
+    [this->webView_ loadFileURL:html_url
+        allowingReadAccessToURL:[html_url URLByDeletingLastPathComponent]];
   }
 
-  auto get_webview() -> WKWebView * { return webView_; }
+  auto attach_to(NSWindow *window) -> void {
+    NSView *contentView = [window contentView];
+    [webView_ setFrame:contentView.bounds];
+    [webView_ setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    [contentView addSubview:webView_];
+  }
 
 private:
   WKWebView *webView_;
@@ -53,12 +65,8 @@ WebView::~WebView() { delete internal_; }
 
 auto WebView::attach_to(sourcemeta::native::Window &window) -> void {
   NSWindow *native_window = static_cast<NSWindow *>(window.handle());
-  WKWebView *webview = internal_->get_webview();
-
-  NSView *contentView = [native_window contentView];
-  [webview setFrame:contentView.bounds];
-  [webview setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-  [contentView addSubview:webview];
+  assert(native_window);
+  internal_->attach_to(native_window);
 }
 
 auto WebView::load_url(const std::string &url) -> void {
