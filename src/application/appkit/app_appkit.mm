b0da291c58c176efba41693685fcba74c8c3a7dd
Tony Gorez <gorez.tony@gmail.com>
2024-11-20 20:31:37 +0100

    refactor(application): remove internal duplicate (#44)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- src/application/appkit/app_appkit.mm
+++ src/application/appkit/app_appkit.mm
@@ -1,12 +1,56 @@
 #include <sourcemeta/native/application.h>
 
-#include "delegate.h"
-
 #import <AppKit/AppKit.h>
 #import <Cocoa/Cocoa.h>
 
+#include <exception>
 #include <functional>
 #include <iostream>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@property std::function<void()> on_ready;
+@property std::function<void(std::exception_ptr)> on_error;
+@end
+
+@implementation AppDelegate
+- (AppDelegate *)initWithCallbacks:(std::function<void()>)on_ready
+                               and:(std::function<void(std::exception_ptr)>)
+                                       on_error {
+  self.on_ready = on_ready;
+  self.on_error = on_error;
+
+  return self;
+}
+
+- (BOOL)applicationShouldHandleReopen:(NSApplication *)sender
+                    hasVisibleWindows:(BOOL)visibleWindows {
+  assert(self.on_ready);
+  assert(self.on_error);
+
+  if (!visibleWindows) {
+    try {
+      (self.on_ready)();
+    } catch (const std::exception &) {
+      std::exception_ptr error = std::current_exception();
+      (self.on_error)(error);
+    }
+  }
+
+  return YES;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+  assert(self.on_ready);
+  assert(self.on_error);
+
+  try {
+    (self.on_ready)();
+  } catch (const std::exception &) {
+    std::exception_ptr error = std::current_exception();
+    (self.on_error)(error);
+  }
+}
+@end
 
 namespace {
 sourcemeta::native::Application *instance_{nullptr};
@@ -19,9 +63,13 @@ public:
   Internal() = default;
   ~Internal() = default;
 
-  auto run_application(std::function<void()> on_start) -> void {
+  auto run_application(std::function<void()> on_start,
+                       std::function<void()> on_ready,
+                       std::function<void(std::exception_ptr)> on_error)
+      -> void {
     NSApplication *application = [NSApplication sharedApplication];
-    AppDelegate *delegate = [[AppDelegate alloc] init];
+    AppDelegate *delegate = [[AppDelegate alloc] initWithCallbacks:on_ready
+                                                               and:on_error];
     [application setDelegate:delegate];
     [application run];
 
@@ -47,13 +95,10 @@ auto Application::run() noexcept -> int {
   assert(!running_);
   running_ = true;
 
-  internal_->run_application([this]() {
-    try {
-      on_start();
-    } catch (...) {
-      on_error(std::current_exception());
-    }
-  });
+  internal_->run_application(
+      std::bind(&Application::on_start, this),
+      std::bind(&Application::on_ready, this),
+      std::bind(&Application::on_error, this, std::placeholders::_1));
 
   return EXIT_SUCCESS;
 }
